8288fc15fbea9f52dbfe98bdd82b7afe9a9eb26b
Guillermo Mazzola <guillermo.mazzola@glovoapp.com>
2023-01-20 17:17:59 +0100

    Fixed configuration cache compatibility on AGP 7.4.0

--- plugin/src/integrationTest/resources/project/build.gradle.kts
+++ plugin/src/integrationTest/resources/project/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 
     if (pluginsClasspath == null) {
         // meant to allow open this test project as a standalone project
-        id("com.android.application") version "7.3.1" apply false
+        id("com.android.application") version "7.4.0" apply false
         kotlin("jvm") version "1.7.21" apply false
         id("io.github.gmazzo.codeowners") version "0.0.0-SNAPSHOT"
 
