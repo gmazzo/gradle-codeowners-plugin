76955e0523603d4c07001a92629fdc27d326af87
Guillermo Mazzola <guillermo.mazzola@glovoapp.com>
2023-01-24 15:28:50 +0100

    Only add `core` dependency if `enabled`

--- plugin/src/integrationTest/resources/project/app/build.gradle.kts
+++ plugin/src/integrationTest/resources/project/app/build.gradle.kts
@@ -10,11 +10,12 @@ android {
 }
 
 androidComponents.onVariants {
-    it.codeOwners.enabled.set(it.buildType == "debug")
+    it.codeOwners.enabled.set(it.buildType != "release")
 }
 
 dependencies {
     implementation(project(":lib1"))
     implementation(project(":lib2"))
+    testReleaseImplementation("io.github.gmazzo.codeowners:core") // required to the test to compile
     testImplementation("junit:junit:4.13.2")
 }
