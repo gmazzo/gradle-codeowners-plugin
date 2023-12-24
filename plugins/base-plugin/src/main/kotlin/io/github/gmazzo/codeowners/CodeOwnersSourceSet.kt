1855e8c28f15ff17aacf2006b019dda42f97b34a
Guillermo Mazzola <gmazzo65@gmail.com>
2023-12-24 18:55:21 +0100

    Added kdoc

--- plugins/base-plugin/src/main/kotlin/io/github/gmazzo/codeowners/CodeOwnersSourceSet.kt
+++ plugins/base-plugin/src/main/kotlin/io/github/gmazzo/codeowners/CodeOwnersSourceSet.kt
@@ -5,10 +5,25 @@ import org.gradle.api.file.ConfigurableFileCollection
 
 interface CodeOwnersSourceSet : Named {
 
+     /**
+      * The source files collection to be analyzed by the CodeOwners tasks.
+      *
+      * Usually you don't need to feed them manually, as they are automatically collected from the build
+      */
      val sources : ConfigurableFileCollection
 
+     /**
+      * The compiled classes files collection to be analyzed by the CodeOwners tasks.
+      *
+      * Usually you don't need to feed them manually, as they are automatically collected from the build
+      */
      val classes : ConfigurableFileCollection
 
+     /**
+      * The external mapping files (.codeowners) collection to be analyzed by the CodeOwners tasks.
+      *
+      * Usually you don't need to feed them manually, as they are automatically collected from the build
+      */
      val mappings : ConfigurableFileCollection
 
 }
