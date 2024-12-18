abc2e7e6f9e089030661ee00f7498f9b69c9a670
Guillermo Mazzola <gmazzo65@gmail.com>
2024-12-19 00:23:01 +0100

    Adopted `importClasses` plugin

--- plugins/matcher/build.gradle.kts
+++ plugins/matcher/build.gradle.kts
@@ -1,13 +1,24 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.importClasses)
     `jvm-convention-module`
     `embedded-dependencies`
 }
 
 description = "CodeOwners Matcher Library"
 
-dependencies {
-    implementation(libs.jgit)
+sourceSets.main {
+    importClasses(libs.jgit) {
+        repackageTo = "io.github.gmazzo.codeowners.matcher.jgit"
+        keep("org.eclipse.jgit.ignore.FastIgnoreRule")
 
+        exclude("META-INF/**/module-info.class")
+        include("**.class")
+        exclude("**.*")
+    }
+}
+
+dependencies {
+    implementation(libs.slf4j) // transitive of libs.jgit, required by FastIgnoreRule
     testImplementation(libs.junit.params)
 }
