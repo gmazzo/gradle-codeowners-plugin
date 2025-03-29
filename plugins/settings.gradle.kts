86524ee496f305bd657d5f16a3fd5df6a18db417
Guillermo Mazzola <gmazzo65@gmail.com>
2025-03-29 23:22:55 +0100

    Update settings.gradle.kts

--- plugins/settings.gradle.kts
+++ plugins/settings.gradle.kts
@@ -1,12 +1,5 @@
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        mavenLocal()
-    }
-}
-
 dependencyResolutionManagement {
     repositoriesMode = RepositoriesMode.PREFER_PROJECT
     repositories {
