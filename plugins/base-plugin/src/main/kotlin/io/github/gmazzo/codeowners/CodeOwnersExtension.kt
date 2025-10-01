54a967ed057e98e1985982b31f0f96e859002d96
Guillermo Mazzola <gmazzo65@gmail.com>
2025-10-01 23:52:05 +0200

    Introduced a dedicated `CodeOwnersExtension` for base plugin

---
+++ plugins/base-plugin/src/main/kotlin/io/github/gmazzo/codeowners/CodeOwnersExtension.kt
@@ -0,0 +1,6 @@
+package org.gradle.kotlin.dsl.io.github.gmazzo.codeowners
+
+import io.github.gmazzo.codeowners.CodeOwnersExtensionBase
+import io.github.gmazzo.codeowners.CodeOwnersSourceSet
+
+interface CodeOwnersExtension : CodeOwnersExtensionBase<CodeOwnersSourceSet>
