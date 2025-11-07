471c14621f03b2a513ba7c56a2521363e327ddad
Guillermo Mazzola <gmazzo65@gmail.com>
2025-11-07 16:26:23 +0100

    Adding public API check (#239)

--- plugins/kotlin-core/build.gradle.kts
+++ plugins/kotlin-core/build.gradle.kts
@@ -1,3 +1,7 @@
+@file:OptIn(ExperimentalAbiValidation::class)
+
+import org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     id("jvm-base-convention-module")
@@ -12,10 +16,15 @@ kotlin {
     iosSimulatorArm64()
     js(IR) { browser(); nodejs() }
     applyDefaultHierarchyTemplate()
+    abiValidation.enabled = true
 }
 
 dependencies {
     commonMainImplementation(libs.kotlin.reflect)
     commonTestImplementation(libs.kotlin.test)
     commonTestImplementation(platform(libs.junit.bom))
+}
+
+tasks.check {
+    dependsOn(tasks.checkLegacyAbi)
 }
