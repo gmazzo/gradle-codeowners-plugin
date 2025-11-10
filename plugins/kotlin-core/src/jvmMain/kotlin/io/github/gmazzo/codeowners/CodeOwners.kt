47eddd6cc33a7e5b22fefd52bbea849838e7adf2
Guillermo Mazzola <gmazzo65@gmail.com>
2025-11-10 11:12:57 +0100

    `Throwable.codeOwners` will abort resolution at any exception (#241)

--- plugins/kotlin-core/src/jvmMain/kotlin/io/github/gmazzo/codeowners/CodeOwners.kt
+++ plugins/kotlin-core/src/jvmMain/kotlin/io/github/gmazzo/codeowners/CodeOwners.kt
@@ -22,14 +22,28 @@ public val KFunction<*>.codeOwners: Set<String>?
 public actual val Throwable.codeOwners: Set<String>?
     get() = getCodeOwners(Thread.currentThread().contextClassLoader)
 
-public fun Throwable.getCodeOwners(classLoader: ClassLoader): Set<String>? =
-    stackTrace.asSequence().mapNotNull { it.getCodeOwners(classLoader) }.firstOrNull()
+public fun Throwable.getCodeOwners(classLoader: ClassLoader): Set<String>? {
+    try {
+        for (element in stackTrace) {
+            val owners = Class.forName(element.className, false, classLoader).codeOwners
 
-private fun StackTraceElement.getCodeOwners(classLoader: ClassLoader) = try {
-    Class.forName(className, false, classLoader).codeOwners
+            if (owners != null) {
+                return owners
+            }
+        }
 
-} catch (e: Throwable) {
-    null
+    } catch (_: Exception) {
+        /**
+         * `Class.forName` produces a crash, which its `fillInStackTrace` will be an expensive operation.
+         *
+         * Since we don't expect to have an unresolvable class in the stack trace, given the stack trace should come
+         * from a valid execution path, keep trying to resolve the remaining class names once 1 has failed would be
+         * probably lead to a system resources starvation (and an ANR on Android, which is why we are doing this change).
+         *
+         * We just assume a panic condition instead, and abort the resolution.
+         */
+    }
+    return null
 }
 
 private fun Class<*>.resolveInterfacesIfProxy() = when {
